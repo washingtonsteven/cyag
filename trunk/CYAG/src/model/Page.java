6c9f45b8ccef136b0ee3e646ecd60dc3aaaadea8
washington.steven@gmail.com <washington.steven@gmail.com@199a1352-482d-11de-8855-c187e5f49513>
2009-05-24 21:54:33 +0000

    Added: 	Documentation to the model package (Javadoc and inline). 	Javadoc HTML files

--- trunk/CYAG/src/model/Page.java
+++ trunk/CYAG/src/model/Page.java
@@ -35,7 +35,7 @@ public class Page
 	private DocumentBuilderFactory dbf;
 	private DocumentBuilder db;
 	private Document doc;
-	File pageData;
+	private File pageData;
 	
 	public Page(int id, String text, String title)
 	{
@@ -58,8 +58,16 @@ public class Page
 		constructPage(id, folder);
 	}
 	
+	/**
+	 * Reads the info from "id.xml" into the Page object.
+	 * If "id.xml" does not exist, it is created with only a &lt;page&gt; element
+	 * 
+	 * @param id The id of the Page to be loaded/created
+	 * @param folder The folder containing id.xml
+	 */
 	public void constructPage(int id, String folder)
 	{
+		//set the file, instantice Linked Lists
 		pageData = new File(folder+id+".xml");
 		choices = new LinkedList<Link>();
 		
@@ -68,19 +76,26 @@ public class Page
 			dbf = DocumentBuilderFactory.newInstance();
 			db = dbf.newDocumentBuilder();
 			
+			//If the page is there, grab data from it
 			if (pageData.exists())
 			{
 				doc = db.parse(pageData);
 				Element root = doc.getDocumentElement();
 				NodeList nodes = root.getChildNodes();
+				//use this in loop instead of nodes.getLength()
+				//As Elements are added, nodes.getLength() gets larger,
+				//and causes an infinite loop
 				int nodeLength = nodes.getLength();
 				
+				//Set the id based on the file
 				this.id = Integer.parseInt(root.getAttribute("id"));
 				
+				//loop though the children
 				for (int i = 0; i < nodeLength; i++)
 				{
 					Element curr = (Element) nodes.item(i);
 					
+					//Get the tag name
 					String tagName = curr.getTagName();
 					
 					if (tagName.equals("title"))
@@ -89,6 +104,7 @@ public class Page
 						text = curr.getTextContent();
 					else if (tagName.equals("choice"))
 					{
+						//Loop to Get the target and link text
 						NodeList linkChildren = curr.getChildNodes();
 						int linkLength = linkChildren.getLength();
 						int tgt = -1;
@@ -108,17 +124,23 @@ public class Page
 								txt = lCurr.getTextContent();
 						}
 						
+						//If we got valid data from the file create the choice
 						if (tgt != -1 && !txt.isEmpty())
 							choices.add(new Link(tgt,txt));
 					}
 				}
 			}
-			else
+			else //if the file doesn't exist
 			{
+				//assign id
 				this.id = id;
+				
+				//Create a new Document
 				doc = db.newDocument();
+				//Create root element...<page> and the id
 				Element root = doc.createElement("page");
 				root.setAttribute("id",id+"");
+				//add <page> to the document
 				doc.appendChild(root);
 			}
 		} catch (Exception e) 
@@ -127,18 +149,24 @@ public class Page
 		}
 	}
 
+	/**
+	 * Setter for the text of this page
+	 * 
+	 * @param text
+	 */
 	public void setText(String text)
 	{
 		this.text = text;
 		NodeList tList = doc.getElementsByTagName("text");
 		
-		if (tList.getLength() > 0)
+		if (tList.getLength() > 0) //there should only be one, so grab the one at item(0)
 			((Element) tList.item(0)).setTextContent(text);
-		else
+		else //<text> isn't there...create it
 		{
 			Element textElem = doc.createElement("text");
 			textElem.setTextContent(text);
 			
+			//append to root
 			doc.getDocumentElement().appendChild(textElem);
 		}
 	}
@@ -148,8 +176,14 @@ public class Page
 		return text;
 	}
 
+	/**
+	 * Setter for the title field
+	 * 
+	 * @param title
+	 */
 	public void setTitle(String title)
 	{
+		//see setText(String) for comments, it's identical
 		this.title = title;
 		NodeList tList = doc.getElementsByTagName("title");
 		
@@ -169,70 +203,102 @@ public class Page
 		return title;
 	}
 	
+	/**
+	 * Adds a choice to the list of choices
+	 * 
+	 * @param choice A Link object describing the choice
+	 * @return true if the choice was added
+	 */
 	public boolean addChoice(Link choice)
 	{
 		Iterator<Link> iter = choices.iterator();
 		
+		//Make sure there are no duplicates
 		while (iter.hasNext())
 			if (iter.next().equals(choice)) return false;
 		
+		//Add it to the internal LinkedList
 		boolean success = choices.add(choice);
 		
 		if (!success) return success;
 		
+		//Create the choice element
 		Element choiceElem = doc.createElement("choice");
 		
+		//if there are no choices, make this the first one
 		if (choices.size() <= 1)
 			choiceElem.setAttribute("id", "1");
-		else
+		else //make this the last one (1 + the id of the last choice)
 			choiceElem.setAttribute("id", (choices.size()+""));
 
+		//Create the choice sub elements <target> and <link_text>
 		Element targetElem = doc.createElement("target");
 		targetElem.setTextContent(choice.getTarget()+"");
 		Element textElem = doc.createElement("link_text");
 		textElem.setTextContent(choice.getText());
 		
+		//Add those children to <choice>
 		choiceElem.appendChild(targetElem);
 		choiceElem.appendChild(textElem);
 		
+		//Add choice to <page>
 		doc.getDocumentElement().appendChild(choiceElem);
 		
 		return success;
 	}
 	
+	/**
+	 * Removes a choice from this page 
+	 * 
+	 * @param choice the choice to be removed
+	 * @return true if the operation was successful
+	 */
 	public boolean removeChoice(Link choice)
 	{
+		//Remove it from the internal LinkedList
 		boolean success = choices.remove(choice);
 		
 		if (!success) return success;
 		
+		//Get all the choices on this page
 		NodeList choiceList = doc.getElementsByTagName("choice");
 		
+		//return false if there are no choices to remove
 		if (choiceList.getLength() <= 0) return false;
 		
+		//Set to false
 		success = false;
+		//loop through choices
 		for (int i = 0; i < choiceList.getLength(); i++)
 		{
 			Element curr = (Element) choiceList.item(i);
+			//Get this choices children, <target> and <link_text>
 			NodeList linkChildren = curr.getChildNodes();
 			
+			//If we already removed a choice, bump up the id of subsequent choices
 			if (success)
 			{
+				//Make sure id is set (should be)
 				if (curr.getAttribute("id").isEmpty()) continue;
 				
+				//Get the id of the current Link
 				int lid = Integer.parseInt(curr.getAttribute("id"));
 				
+				//Set the id to that id minus one
 				curr.setAttribute("id",(lid-1)+"");
 			}
 			
-			if (Integer.parseInt(linkChildren.item(0).getTextContent()) == choice.getTarget() && linkChildren.item(1).getTextContent().equals(choice.getText()))
+			//If the current element is equal to the given one
+			if (new Link(Integer.parseInt(linkChildren.item(0).getTextContent()),linkChildren.item(1).getTextContent()).equals(choice))
 			{
+				//remove the choice from its parent 
 				curr.getParentNode().removeChild(curr);
+				//Set to true, so subsequent choices' id can be updated
 				success = true;
 			}
 		}
 		
-		return false;
+		return success;
 	}
 	
 	/**
@@ -254,8 +320,10 @@ public class Page
 	
 	private boolean saveXML() throws Exception
 	{	
+		//if the tree is empty
 		if (doc.getChildNodes().getLength() <= 0)
 		{
+			//Set up all the Elements
 			Element pageElem = doc.createElement("page");
 			pageElem.setAttribute("id",id+"");
 			
@@ -265,10 +333,12 @@ public class Page
 			Element titleElem = doc.createElement("title");
 			titleElem.setTextContent(title);
 			
+			//Add them to the document
 			doc.appendChild(pageElem);
 			pageElem.appendChild(titleElem);
 			pageElem.appendChild(textElem);
 			
+			//Loop over the choices
 			Iterator<Link> iter = choices.iterator();
 			int i = 1;
 			
@@ -276,23 +346,29 @@ public class Page
 			{
 				Link curr = iter.next();
 				
+				//Create the choice element
 				Element choiceElem = doc.createElement("choice");
 				choiceElem.setAttribute("id", i+"");
 				
+				//create the target element
 				Element targetElem = doc.createElement("target");
 				targetElem.setTextContent(curr.getTarget()+"");
 				
+				//create the link text element
 				Element linkTextElem = doc.createElement("link_text");
 				linkTextElem.setTextContent(curr.getText());
 				
+				//append link text and target to choice
 				choiceElem.appendChild(targetElem);
 				choiceElem.appendChild(linkTextElem);
+				//append choice to root
 				pageElem.appendChild(choiceElem);
 				
 				i++;
 			}
 		}
 		
+		//Write the document to file
 		TransformerFactory tf = TransformerFactory.newInstance(); 
 		Transformer trans = tf.newTransformer(); 
 
@@ -326,34 +402,47 @@ public class Page
 		return choices;
 	}
 	
+	/**
+	 * Gets a choice at index (i) based on the XML data
+	 * 
+	 * @param index
+	 * @return a Link describing the choice at index (i)
+	 */
 	public Link getChoice(int index)
 	{
+		//Get all choices
 		NodeList choices = doc.getElementsByTagName("choice");
 		
+		//loop through choices
 		for (int i = 0; i < choices.getLength(); i++)
 		{
 			Element curr = (Element) choices.item(i);
 			
+			//if the current choice is equal to the one given
 			if (curr.getAttribute("id").equals(index+""))
 			{
+				//Get the children of the choice
 				NodeList linkChildren = curr.getChildNodes();
 				String txt = "";
 				int tgt = -1;
 				
+				//loop over them
 				for (int j = 0; j < linkChildren.getLength(); j++)
 				{
+					//Get the data in the child elements
 					Element lCurr = (Element) linkChildren.item(j);
 					String tagName = lCurr.getTagName();
 					
-					if (tagName.equals("target"))
+					if (tagName.equals("target")) //set target
 					{
 						if (!lCurr.getTextContent().isEmpty())
 						tgt = Integer.parseInt(lCurr.getTextContent());
 					}
-					else
+					else //set text
 						txt = lCurr.getTextContent();
 				}
 				
+				//if data is valid, return a new link
 				if (tgt != -1 && !txt.isEmpty())
 					return new Link(tgt, txt);
 			}
